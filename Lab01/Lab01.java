a23171963527771304e1dc5a0c301d633a716f85
DenisTmenov <denis.tmenov@gmail.com>
2018-02-02 22:47:41 +0300

    Add files via upload

---
+++ Lab01/Lab01.java
@@ -0,0 +1,8 @@
+public class Lab01{
+	public static void main(String[] args){
+		System.out.println("Ф.И.О. студента \t" + "Тменов Денис Сергеевич");
+		System.out.println("Номер группы \t" + "ПОИС1709в.2");
+		System.out.println("Основной вид деятельности \t" + "программирование на Java (freelance)");
+		System.out.println("Увлечения \t" + "Плавание, путешествия, горные лыжи, роликовые коньки(freestyle)");
+	}
+}
